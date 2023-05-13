961fac091085f1bb2f2352d374262f0c6103f2ef
worthless443 <aissypewdiepie@gmail.com>
2023-05-14 01:02:32 +0530

    multi-color option

---
+++ uninit_opt.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void nigger() {void *p = NULL; return *p;}
+int main() {
+	nigger();
+	int i = 1,j = 2,l = 3,m = 4;
+	char *nig = "n";
+	char *tran = "t";
+	printf("%d\n",i);
+	printf(nig);
+	printf(tran);
+}
